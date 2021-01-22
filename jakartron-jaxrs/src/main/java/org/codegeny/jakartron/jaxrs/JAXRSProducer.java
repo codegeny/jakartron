ffe1afddcef8587316adb0d4d0067fd625ce492b
Xavier Dury <xavier@codegeny.org>
2021-01-22 17:40:37 +0100

    Added JAXRS client producer

--- jakartron-jaxrs/src/main/java/org/codegeny/jakartron/jaxrs/JAXRSProducer.java
+++ jakartron-jaxrs/src/main/java/org/codegeny/jakartron/jaxrs/JAXRSProducer.java
@@ -9,9 +9,9 @@ package org.codegeny.jakartron.jaxrs;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,20 +20,21 @@ package org.codegeny.jakartron.jaxrs;
  * #L%
  */
 
-import org.codegeny.jakartron.AdditionalClasses;
-import org.codegeny.jakartron.servlet.EnableServlet;
-import org.codegeny.jakartron.validation.EnableValidation;
-import org.jboss.resteasy.cdi.ResteasyCdiExtension;
+import javax.enterprise.context.Dependent;
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+@Dependent
+public final class JAXRSProducer {
 
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
-@EnableServlet
-@EnableValidation
-@AdditionalClasses({JAXRSIntegration.class, ResteasyCdiExtension.class, JAXRSProducer.class})
-public @interface EnableJAXRS {
+    @Produces
+    public Client newClient() {
+        return ClientBuilder.newClient();
+    }
+
+    public void closeClient(@Disposes Client client) {
+        client.close();
+    }
 }
