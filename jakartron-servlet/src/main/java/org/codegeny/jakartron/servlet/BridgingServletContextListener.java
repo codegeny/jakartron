8a0e46a80d998cff2928f3597e0cb1f674b907cb
Xavier Dury <xavier@codegeny.org>
2021-05-05 16:39:22 +0200

    Updated license

--- jakartron-servlet/src/main/java/org/codegeny/jakartron/servlet/BridgingServletContextListener.java
+++ jakartron-servlet/src/main/java/org/codegeny/jakartron/servlet/BridgingServletContextListener.java
@@ -4,7 +4,7 @@ package org.codegeny.jakartron.servlet;
  * #%L
  * jakartron-servlet
  * %%
- * Copyright (C) 2018 - 2020 Codegeny
+ * Copyright (C) 2018 - 2021 Codegeny
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
