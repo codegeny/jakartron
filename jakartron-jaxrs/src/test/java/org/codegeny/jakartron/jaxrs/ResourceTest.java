14b4d16573dbbf491fca43052c63e12f0f5973c9
Xavier Dury <xavier@codegeny.org>
2022-01-25 22:01:22 +0100

    Added resteasy-client

--- jakartron-jaxrs/src/test/java/org/codegeny/jakartron/jaxrs/ResourceTest.java
+++ jakartron-jaxrs/src/test/java/org/codegeny/jakartron/jaxrs/ResourceTest.java
@@ -30,9 +30,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Set;
 
 import static io.restassured.RestAssured.given;
